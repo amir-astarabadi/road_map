e6605f9ccd602fb47f9e1f61fc374adf9e54bc96
amir.pourastarabadi <amir.pourastarabadi@shab.ir>
2024-07-23 17:50:52 +0330

    move careeirs before auth

--- modules/RoadMap/Routes/api.php
+++ modules/RoadMap/Routes/api.php
@@ -8,10 +8,10 @@ use Modules\RoadMap\Controllers\PersonalPreferenceController;
 use Modules\RoadMap\Controllers\ProfileContoller;
 use Modules\RoadMap\Controllers\SuggestionContoller;
 
+Route::apiResource('careers', CareerController::class)->only(['index']);
 Route::middleware('auth:sanctum')->group(function(){
     Route::apiResource('personal-preference', PersonalPreferenceController::class)->except('update');
     Route::put('personal-preference/{personal_preference?}', [PersonalPreferenceController::class, 'update'])->name('personal-preference.update');
-    Route::apiResource('careers', CareerController::class)->only(['index']);
     Route::post('courses', [CourseController::class, 'store'])->name('courses.store');
     Route::apiResource('exams', ExamController::class)->only(['store', 'update']);
     Route::get('suggestions', [SuggestionContoller::class, 'show'])->name('suggestions.show');
