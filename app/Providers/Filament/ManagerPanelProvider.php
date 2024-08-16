6d19b6a136b04b00c2ee7a5ea6957bf039e16f32
amir.pourastarabadi <amir.pourastarabadi@shab.ir>
2024-08-16 09:32:40 +0330

    read panel fonts from google

--- app/Providers/Filament/ManagerPanelProvider.php
+++ app/Providers/Filament/ManagerPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Filament\FontProviders\GoogleFontProvider;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -26,6 +27,7 @@ class ManagerPanelProvider extends PanelProvider
             ->default()
             ->id('manager')
             ->path('manager')
+            ->font('Inter', provider: GoogleFontProvider::class)
             ->login()
             ->colors([
                 'primary' => Color::Amber,
