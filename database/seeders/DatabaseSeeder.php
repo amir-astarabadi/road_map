7dca3eb253817382e3d8128275b99705c8916e64
Amir Pourastarabadi <amir.astarabadi.dev@gmail.com>
2025-03-09 15:21:46 +0330

    add seeders to databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Modules\Authentication\Models\User;
+use Modules\RoadMap\Database\Seeders\CareerSeeder;
+use Modules\RoadMap\Database\Seeders\QuestionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-       
+        $this->call([
+            CareerSeeder::class,
+            QuestionSeeder::class,
+        ]);
     }
 }
